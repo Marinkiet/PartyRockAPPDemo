10d8cd3071bd313bd3bab70a6dc5dcbb8a04c504
Marinkie <tmarinkie12@gmail.com>
2024-03-11 19:54:07 +0200

    init: Homescreen layout comments

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      {/* Header */}
+      {/* Subheader */}
+      {/* SubjectsCard */}
+      {/* Subheader */}
+      {/* RecentCard */}
+     
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        // backgroundColor:'blue'
+      }
+});
