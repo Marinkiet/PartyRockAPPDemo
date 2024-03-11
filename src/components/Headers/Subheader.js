a303ec8fc58708ca4c34499c669202ebc01d9baa
Marinkie <tmarinkie12@gmail.com>
2024-03-11 20:03:33 +0200

    init: Subheader

---
+++ src/components/Headers/Subheader.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Subheader = () => {
+  return (
+    <View>
+      <Text>Subheader</Text>
+    </View>
+  );
+};
+
+export default Subheader;
+
+const styles = StyleSheet.create({});
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,23 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import MainHeader from "../components/Headers/MainHeader";
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      {/* MainHeader */}
-      {/* Subheader */}
-      {/* SubjectsCard */}
-      {/* Subheader */}
-      {/* RecentCard */}
-     
+        <View style={styles.headerWrapper}>
+            {/* MainHeader */}
+            <MainHeader/>
+        </View>
+        <View style={styles.subjectWrapper}>
+            {/* Subheader */}
+            {/* SubjectsCard */}
+        </View>
+        <View style={styles.ReactCardWrapper}>
+             {/* Subheader */}
+             {/* RecentCard */}
+        </View>
+    
     </View>
   );
 };
@@ -19,6 +27,18 @@ export default HomeScreen;
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        // backgroundColor:'blue'
+        backgroundColor:'magenta'
+      },
+      headerWrapper:{
+        flex:1,
+        backgroundColor:'blue'
+      },
+      subjectWrapper:{
+        flex:2,
+        backgroundColor:'pink'
+      },
+      ReactCardWrapper:{
+        flex:3,
+        backgroundColor:'orange'
       }
 });
